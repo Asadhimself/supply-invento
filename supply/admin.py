eee8b85b9ca2c5688722ee9b3ff1d3a2679e39d4
Asad F <theavenger1712@gmail.com>
2024-02-27 13:57:49 +0500

    Admin site modifications

--- supply/admin.py
+++ supply/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import CustomUser, OrderTable
 from django.contrib.auth.admin import UserAdmin
@@ -14,20 +14,35 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Permissions", {"fields": ("is_staff", "is_active")}),
-        ("User info", {"fields": ("role", "user_class")})
+        ("User info", {"fields": ("role", "user_class")}),
     )
     add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": (
-                "email", "password1", "password2", "is_staff",
-                "is_active", "role", "user_class"
-            )}
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "role",
+                    "user_class",
+                ),
+            },
         ),
     )
     search_fields = ("email",)
     ordering = ("email",)
 
 
+admin.site.unregister(Group)
+
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(OrderTable)
+
+#site visuals
+admin.site.site_header = "Supply Administration"
+admin.site.site_title = "Supply Invento"
+admin.site.index_title = "Supply Invento"
