e993330f7e671cffc5f80d346d4dff2e02ccd4f3
Asad F <theavenger1712@gmail.com>
2024-02-29 09:55:44 +0500

    Login views & template

--- supply/views.py
+++ supply/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'supply/login.html', {'error_message': 'Invalid email/password'})    
+
+    return render(request, 'supply/login.html')
+
+@login_required(login_url='/login')
+def home_view(request):
+    return HttpResponse("Hello world")
