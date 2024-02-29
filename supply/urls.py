2bf947e1f12bd7532a970d695d4bbf9c882fec77
Asad F <theavenger1712@gmail.com>
2024-02-29 10:03:21 +0500

    Logout view & base html

--- supply/urls.py
+++ supply/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from supply.views import login_view, home_view
+from supply.views import login_view, home_view, logout_view
 
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ]
--- supply/views.py
+++ supply/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -6,18 +6,28 @@ from django.shortcuts import redirect, render
 
 def login_view(request):
     if request.method == "POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        email = request.POST.get("email")
+        password = request.POST.get("password")
         user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            return render(request, 'supply/login.html', {'error_message': 'Invalid email/password'})    
+            return render(
+                request,
+                "supply/login.html",
+                {"error_message": "Invalid email/password"},
+            )
 
-    return render(request, 'supply/login.html')
+    return render(request, "supply/login.html")
 
-@login_required(login_url='/login')
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required(login_url="/login")
 def home_view(request):
-    return HttpResponse("Hello world")
+    return render(request, "supply/home.html")
